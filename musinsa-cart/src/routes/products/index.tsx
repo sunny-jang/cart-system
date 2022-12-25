61f13428dca4f90cbdf99de7e1a5a5737e74ebe3
sunny-jang <esunbest@naver.com>
2022-12-25 11:47:51 +0900

    리스트 갯수별 나누는 함수 추가

--- musinsa-cart/src/routes/products/index.tsx
+++ musinsa-cart/src/routes/products/index.tsx
@@ -1,17 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { productItems } from '../../data/data.js';
+import styles from './products.module.scss';
+
 
 export interface IAppProps {}
 
+interface Product {
+	item_no: number,
+  item_name: string,
+  detail_image_url: string,
+  price: number,
+  score: number,
+}
 export default function App ({}: IAppProps) {
+  const [divNum, setDivNum] = useState(5);
+  const [page, setPage] = useState(1);
+  const [list, setList] = useState<Product[][]>([]);
+
+  const chunk = (list:Array<Product>, size:number)=> {
+    let _list:Array<Array<Product>> = [];
+    for (let i = 0; i < list.length; i += size) {
+      _list.push(list.slice(i, i + size));
+    }
+    return _list;
+  }
+  
+  useEffect(()=>{
+    setList(chunk(productItems, divNum));
+    console.log(list)
+  },[]);
+
   return (
-    <div>
+    <div className={styles.products}>
       {
-        productItems.map((product, index)=>{
-            return <div key={index}>
-                <div>{product.price}</div>
-                <div><img src={product.detail_image_url} /></div>
-                <div>{product.item_name}</div>
+        list?.[0]?.map((product, index)=>{
+            return <div key={index} className={styles.product}>
+                <div className={styles.productThumbnail}><img src={product.detail_image_url} /></div>
+                <div className={styles.productInfo}>
+                  <div className={styles.productName}>{product.item_name}</div>
+                  <div className={styles.productPrice}>{product.price}원</div>
+                </div>
             </div>
         })
       }
