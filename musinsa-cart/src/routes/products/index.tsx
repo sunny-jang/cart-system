11758329fc7fe309b9e9c216b4d2b50778f5998d
sunny-jang <esunbest@naver.com>
2022-12-25 13:42:44 +0900

    카트 추가 훅 생성

--- musinsa-cart/src/routes/products/index.tsx
+++ musinsa-cart/src/routes/products/index.tsx
@@ -12,10 +12,41 @@ interface Product {
   price: number,
   score: number,
 }
+
+const useCartState = ()=>{
+  let [cart, setCart] = useState(JSON.parse(localStorage.getItem("cart")+''));
+  
+  useEffect(()=>{
+    localStorage.setItem("cart", JSON.stringify(cart));
+  },[cart])
+  
+  const modifyCart = (itemNo:number) => {
+    if(!cart) {
+      setCart([itemNo]);
+    }
+    else  {
+      if(cart.includes(itemNo)) {
+        let _cart = [...cart];
+        _cart.splice(cart.indexOf(itemNo),1);
+        setCart(_cart)
+      } else {
+        if(cart.length >=3) {
+          alert("최대 3개의 상품을 장바구니에 담을 수 있습니다.");
+          return;
+        }
+        setCart([...cart, itemNo]);
+      }
+    }
+  }
+
+  return [cart, modifyCart];
+}
+
 export default function App ({}: IAppProps) {
   const [divNum, setDivNum] = useState(5);
   const [page, setPage] = useState(1);
   const [list, setList] = useState<Product[][]>([]);
+  const [cart, setCart] = useCartState();
 
   const chunk = (list:Array<Product>, size:number)=> {
     let sortedList = list.sort((a,b) => a.score-b.score);
@@ -39,6 +70,8 @@ export default function App ({}: IAppProps) {
                 <div className={styles.productInfo}>
                   <div className={styles.productName}>{product.item_name}</div>
                   <div className={styles.productPrice}>{product.price}원</div>
+
+                  <button onClick={()=>{setCart(product.item_no)}}>{cart?.includes(product.item_no) ? '빼기' : '담기'}</button>
                 </div>
             </div>
         })
