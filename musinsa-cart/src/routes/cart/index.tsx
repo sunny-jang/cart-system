a9e8d9ff06aee8db074611c5a122bba390bc9e9a
sunny-jang <esunbest@naver.com>
2022-12-26 22:22:13 +0900

    선택되어있는 상품 렌더링

--- musinsa-cart/src/routes/cart/index.tsx
+++ musinsa-cart/src/routes/cart/index.tsx
@@ -1,22 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import Product from '../../components/product/Products';
 import { productItems, coupons } from '../../data/data.js';
-import styles from './products.module.scss';
+import styles from './index.module.scss';
+import _ from 'lodash';
+import Product from '../../components/product/Products';
+import Products from '../products/index.js';
 
 export interface IAppProps {}
 
-export interface Coupon {
-    type: string,
-    title: string,
-    discountRate?: number,
-    discountAmount?: number
-}
-
 export default function Cart ({}: IAppProps) {
+  const [selectedItems , setSelectedItems] = useState([]);
+  const [cartItems, setCartItems] = useState<Array<Product>>([]);
+  useEffect(()=>{
+    let addedItems = JSON.parse(localStorage.getItem("cart")+'');
+    let items: Product[] = addedItems.map((item: number)=>{
+      return _.find(productItems, { item_no: item });
+    });
+
+    setCartItems(items);
+  },[])
   
   return (
     <div className={styles.cart}>
-      
+      {
+        cartItems.map((item, index)=>{
+          return  <Product product={item} />
+        })
+      }
     </div>
   );
 }
