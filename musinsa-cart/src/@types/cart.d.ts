2cbf6f41a6a2b0753a46ece1798e948bffd1bce6
sunny-jang <esunbest@naver.com>
2022-12-26 23:50:42 +0900

    장바구니 아이템 타입 선언

--- musinsa-cart/src/@types/cart.d.ts
+++ musinsa-cart/src/@types/cart.d.ts
@@ -1,17 +1,21 @@
 declare module 'cart-types';
 
 interface Product {
-    item_no: number,
-    item_name: string,
-    detail_image_url: string,
-    price: number,
-    score: number,
+    item_no: number;
+    item_name: string;
+    detail_image_url: string;
+    price: number;
+    score: number;
     availableCoupon?: boolean | undefined;
   }
 
+interface CartItem extends Product {
+  amount: number;
+}
+
 interface Coupon {
-  type: string,
-  title: string,
-  discountRate?: number,
-  discountAmount?: number
+  type: string;
+  title: string;
+  discountRate?: number;
+  discountAmount?: number;
 }
