5c27739d1176a622367aa0d684fe62f497ea0f88
sunny-jang <esunbest@naver.com>
2022-12-27 00:23:44 +0900

    선택 상품 수량 변경 함수 추가

--- musinsa-cart/src/routes/cart/index.tsx
+++ musinsa-cart/src/routes/cart/index.tsx
@@ -8,22 +8,59 @@ import Products from '../products/index.js';
 export interface IAppProps {}
 
 export default function Cart ({}: IAppProps) {
-  const [selectedItems , setSelectedItems] = useState([]);
-  const [cartItems, setCartItems] = useState<Array<Product>>([]);
+  const [selectedItems, setSelectedItems] = useState<Array<CartItem>>([]);
+  const [cartItems, setCartItems] = useState<Array<CartItem>>([]);
+
   useEffect(()=>{
     let addedItems = JSON.parse(localStorage.getItem("cart")+'');
-    let items: Product[] = addedItems.map((item: number)=>{
+    let items: CartItem[] = addedItems.map((item: number)=>{
       return _.find(productItems, { item_no: item });
     });
 
     setCartItems(items);
-  },[])
+  },[]);
+
+  const changeSelectedItems = (item:CartItem) => {
+    if(_.find(selectedItems, {item_no: item.item_no})) {
+      _.remove(selectedItems, (i)=>i.item_no === item.item_no);
+      setSelectedItems([...selectedItems])
+
+    } else {
+      setSelectedItems([...selectedItems, item])
+    }
+  }
+
+  const changeAmount = (item: CartItem, operater: string)=>{
+      let _item = _.find(selectedItems, {item_no: item.item_no});
+      
+      if(_item) {
+        if(!_item.amount) _item.amount = 1;
+        if(operater === 'plus') {_item.amount += 1}
+        else if(operater === 'minus'){
+          if(_item.amount >= 2) _item.amount -= 1
+        }
+
+        _.remove(selectedItems, {item_no: item.item_no});
+        setSelectedItems([...selectedItems, _item])
+      }
+    }
   
   return (
     <div className={styles.cart}>
       {
         cartItems.map((item, index)=>{
-          return  <Product product={item} />
+          return (
+            <div className={styles.cartItem}>
+              <input 
+                type="checkbox"
+                checked={_.find(selectedItems, {item_no: item.item_no})? true: false}
+                onChange={()=>changeSelectedItems(item)} />
+              <Product product={item} />
+              <button onClick={()=>changeAmount(item, 'plus')}>+</button>
+              <span>{item.amount ? item.amount : 1}</span>
+              <button onClick={()=>changeAmount(item, 'minus')}>-</button>
+            </div>
+            )
         })
       }
     </div>
