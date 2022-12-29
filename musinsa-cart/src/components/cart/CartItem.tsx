d3d492a4bf9618fc688d116b72ff5fb35b39519c
sunny-jang <esunbest@naver.com>
2022-12-29 22:49:31 +0900

    장바구니 아이템 컴포넌트 분리

---
+++ musinsa-cart/src/components/cart/CartItem.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../product/Products';
+import styles from './CartItem.module.scss';
+
+export interface IAppProps {
+  item: CartItem,
+  isChecked: boolean,
+  changeSelectedItems :(item: CartItem)=>void,
+  changeAmount :(item: CartItem, type:string)=>void,
+}
+
+export default function CartItem ({item, isChecked, changeSelectedItems, changeAmount}: IAppProps) {
+ 
+ 
+  return (
+    <div className={styles.cartItem}>
+              <input 
+                className={styles.checkbox}
+                type="checkbox"
+                checked={isChecked}
+                onChange={()=>changeSelectedItems(item)} />
+              <Product product={item} />
+              
+              <button 
+                className={styles.countButton} 
+                disabled={!isChecked}
+                onClick={()=>changeAmount(item, 'plus')}>+
+              </button>
+              <span>{item.amount ? item.amount : 1}</span>
+              <button 
+                className={styles.countButton} 
+                disabled={!isChecked}
+                onClick={()=>changeAmount(item, 'minus')}>-
+              </button>
+            </div>
+  )
+}
--- musinsa-cart/src/routes/cart/index.tsx
+++ musinsa-cart/src/routes/cart/index.tsx
@@ -2,19 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { productItems, coupons } from '../../data/data.js';
 import styles from './index.module.scss';
 import _ from 'lodash';
-import Product from '../../components/product/Products';
-import classnames from 'classnames';
+import Calculator from '../../components/cart/Calculator';
+import CartItem from '../../components/cart/CartItem';
 
 export interface IAppProps {}
 
 export default function Cart ({}: IAppProps) {
   const [selectedItems, setSelectedItems] = useState<Array<CartItem>>([]);
   const [cartItems, setCartItems] = useState<Array<CartItem>>([]);
-  const [totalPrice, setTotalPrice] = useState(0);
-  const [totalDiscount, setTotalDiscount] = useState(0);
-  const [priceBeforeDC, setPriceBeforeDC] = useState(0);
-  const [isCouponOpen, setIsCouponOpen] = useState(false);
-  const [selectedCoupon, setSelectedCoupon] = useState<Coupon>()
 
   useEffect(()=>{
     let addedItems = JSON.parse(localStorage.getItem("cart")+'');
@@ -24,49 +19,6 @@ export default function Cart ({}: IAppProps) {
 
     setCartItems(items);
   },[]);
-
-  useEffect(()=>{
-    let sum = 0;
-    selectedItems.map((item)=>{
-      sum += ((item.amount) * item.price)
-    });
-
-    setPriceBeforeDC(sum);
-  },[selectedItems])
-
-  useEffect(()=>{
-    let availableSum = 0;
-    let notAvailableSum = 0;
-    let totalDC = 0;
-    let sum = 0;
-
-    if(selectedCoupon) {
-      selectedItems.map(item=>{
-        if(item.availableCoupon!==false) {
-          availableSum += (item.price * item.amount);
-        }else {
-          notAvailableSum += (item.price * item.amount);
-        }
-      });
-
-      if(selectedCoupon.discountRate) {
-        sum += (availableSum * (1-(selectedCoupon.discountRate/100)));
-        setTotalDiscount(availableSum * ((selectedCoupon.discountRate/100)));
-      }else if(selectedCoupon.discountAmount) {
-        if(availableSum !== 0) {
-          sum += (availableSum - selectedCoupon.discountAmount);
-          setTotalDiscount(selectedCoupon.discountAmount);
-        };
-      }
-
-    } else {
-        selectedItems.map((item)=>{
-          sum += ((item.amount) * item.price)
-        });
-    }
-
-    setTotalPrice(sum + notAvailableSum);
-  },[selectedItems, selectedCoupon])
 
   const changeSelectedItems = (item:CartItem) => {
     if(_.find(selectedItems, {item_no: item.item_no})) {
@@ -101,51 +53,17 @@ export default function Cart ({}: IAppProps) {
       {
         cartItems.map((item, index)=>{
           const isChecked = _.find(selectedItems, {item_no: item.item_no})? true: false;
+          const cartItem = {item, isChecked, changeAmount, changeSelectedItems};
+          
           return (
-            <div className={styles.cartItem} key={index}>
-              <input 
-                className={styles.checkbox}
-                type="checkbox"
-                checked={isChecked}
-                onChange={()=>changeSelectedItems(item)} />
-              <Product product={item} />
-              
-              <button 
-                className={styles.countButton} 
-                disabled={!isChecked}
-                onClick={()=>changeAmount(item, 'plus')}>+
-              </button>
-              <span>{item.amount ? item.amount : 1}</span>
-              <button 
-                className={styles.countButton} 
-                disabled={!isChecked}
-                onClick={()=>changeAmount(item, 'minus')}>-
-              </button>
-            </div>
+            <CartItem
+              {...cartItem}
+             />
             )
         })
       }
       </div>
-      <div>
-        상품 금액 : {priceBeforeDC}원<br/>
-        할인된 금액 : {totalDiscount}원<br/>
-        결제 금액 : {totalPrice}원<br/>
-        {selectedCoupon && <span>선택된 쿠폰 : {selectedCoupon?.title}<br/></span>}
-        <button onClick={()=>{setIsCouponOpen(!isCouponOpen)}}>쿠폰사용</button><br/>
-        <div 
-          className={classnames(styles.couponList,{[styles.isShow]: isCouponOpen})}>
-          {coupons.map((coupon,index) => {
-            return <div 
-              onClick={()=>{
-                setSelectedCoupon(coupon);
-                setIsCouponOpen(!isCouponOpen)
-              }}
-              className={styles.coupon} 
-              key={index}>{coupon.title}
-            </div>
-          })}
-        </div>
-      </div>
+      <Calculator selectedItems={selectedItems} />
     </div>
   );
 }
