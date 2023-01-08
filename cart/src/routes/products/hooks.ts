31ae18d2261cd9b31cbc91e30f6f37a9213eb69c
sunny-jang <esunbest@naver.com>
2023-01-09 00:11:53 +0900

    함수 분리 및 리팩토링

--- cart/src/routes/products/hooks.ts
+++ cart/src/routes/products/hooks.ts
@@ -1,5 +1,20 @@
 import { useState, useEffect } from "react";
 
+const removeItem = (cart: number[], itemNo: number) => {
+  let _cart = [...cart];
+  _cart.splice(cart.indexOf(itemNo),1);
+
+  return _cart;
+}
+
+const isOverItems = (cart: number[], limit: number) => {
+  if(cart.length >=limit) {
+    alert(`최대 ${limit}개의 상품을 장바구니에 담을 수 있습니다.`);
+    return true;
+  }
+  return false;
+}
+
 export const useCartState = ()=>{
   const [cart, setCart] = useState(JSON.parse(localStorage.getItem("cart")+''));
   
@@ -13,14 +28,9 @@ export const useCartState = ()=>{
     }
     else  {
       if(cart.includes(itemNo)) {
-        let _cart = [...cart];
-        _cart.splice(cart.indexOf(itemNo),1);
-        setCart(_cart)
+        setCart(removeItem(cart, itemNo));
       } else {
-        if(cart.length >=3) {
-          alert("최대 3개의 상품을 장바구니에 담을 수 있습니다.");
-          return;
-        }
+        if(isOverItems(cart, 3)) return;
         setCart([...cart, itemNo]);
       }
     }
